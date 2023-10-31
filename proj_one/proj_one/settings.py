aef7d2297d4a7b85dc199d318603a71888d5831e
CyberMaiber <89603380989@mail.ru>
2023-10-31 22:56:43 +0300

    aftes adaptation

--- proj_one/proj_one/settings.py
+++ proj_one/proj_one/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lp%y_3d%a3e6(9xe66d69f^jlab&g+%5+6chnuhhx*$v-+2s7k'
+#SECRET_KEY = 'django-insecure-lp%y_3d%a3e6(9xe66d69f^jlab&g+%5+6chnuhhx*$v-+2s7k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ['203.0.113.90',
+
+ALLOWED_HOSTS = [
                  '127.0.0.1',
-                 '*']
+                 'CyberMaiber.pythonanywhere.com',
+                 ]
 
 
 # Application definition
@@ -83,8 +88,15 @@ WSGI_APPLICATION = 'proj_one.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CyberMaiber$default',
+        'USER': CyberMaiber,
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'CyberMaiber.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -126,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
 STATIC_APP = BASE_DIR / 'hometask_app/static'
 
 
